de0e023c43d3c7268331a475e83f271f328444a4
carneirofc <claudiofcarneiro@hotmail.com>
2019-08-26 09:54:14 -0300

    iocBoot: Fix digi serial port name

---
+++ iocBoot/iocTopCon/config-node.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+set -x
+set -e
+# Dipole
+/usr/bin/dgrp_cfg_node init 1  10.128.125.101 1
+/usr/bin/dgrp_cfg_node init 2  10.128.125.102 1
+/usr/bin/dgrp_cfg_node init 3  10.128.125.103 1
+/usr/bin/dgrp_cfg_node init 4  10.128.125.104 1
+/usr/bin/dgrp_cfg_node init 5  10.128.125.105 1
+/usr/bin/dgrp_cfg_node init 6  10.128.125.106 1
+/usr/bin/dgrp_cfg_node init 7  10.128.125.107 1
+/usr/bin/dgrp_cfg_node init 8  10.128.125.108 1
+/usr/bin/dgrp_cfg_node init 9  10.128.125.109 1
+/usr/bin/dgrp_cfg_node init 10 10.128.125.110 1
+/usr/bin/dgrp_cfg_node init 11 10.128.125.111 1
+/usr/bin/dgrp_cfg_node init 12 10.128.125.112 1
+/usr/bin/dgrp_cfg_node init 13 10.128.125.113 1
+/usr/bin/dgrp_cfg_node init 14 10.128.125.114 1
+/usr/bin/dgrp_cfg_node init 15 10.128.125.115 1
+/usr/bin/dgrp_cfg_node init 16 10.128.125.116 1
+
+
+# Quadrupoles
+/usr/bin/dgrp_cfg_node init 17 10.128.125.117 1
+/usr/bin/dgrp_cfg_node init 18 10.128.125.118 1
+/usr/bin/dgrp_cfg_node init 19 10.128.125.119 1
+/usr/bin/dgrp_cfg_node init 20 10.128.125.120 1
+/usr/bin/dgrp_cfg_node init 21 10.128.125.121 1
+/usr/bin/dgrp_cfg_node init 22 10.128.125.122 1
+/usr/bin/dgrp_cfg_node init 23 10.128.125.123 1
+/usr/bin/dgrp_cfg_node init 24 10.128.125.124 1
+/usr/bin/dgrp_cfg_node init 25 10.128.125.125 1
+/usr/bin/dgrp_cfg_node init 26 10.128.125.126 1
+/usr/bin/dgrp_cfg_node init 27 10.128.125.127 1
+/usr/bin/dgrp_cfg_node init 28 10.128.125.128 1
+/usr/bin/dgrp_cfg_node init 29 10.128.125.129 1
+/usr/bin/dgrp_cfg_node init 30 10.128.125.130 1
+/usr/bin/dgrp_cfg_node init 31 10.128.125.131 1
+/usr/bin/dgrp_cfg_node init 32 10.128.125.132 1
+
+# Quadrupoles
+/usr/bin/dgrp_cfg_node init 33 10.128.125.133 1
+/usr/bin/dgrp_cfg_node init 34 10.128.125.134 1
+/usr/bin/dgrp_cfg_node init 35 10.128.125.135 1
+/usr/bin/dgrp_cfg_node init 36 10.128.125.136 1
+/usr/bin/dgrp_cfg_node init 37 10.128.125.137 1
+/usr/bin/dgrp_cfg_node init 38 10.128.125.138 1
+/usr/bin/dgrp_cfg_node init 39 10.128.125.139 1
+/usr/bin/dgrp_cfg_node init 40 10.128.125.140 1
+
--- iocBoot/iocTopCon/gen.py
+++ iocBoot/iocTopCon/gen.py
@@ -38,50 +38,50 @@ dbLoadRecords("db/TopConMaster.db", "DEVICE=${PV}, PORT=${PORT}")''')
 
 rega = [
     {'file':'st-dipoles.cmd', 'items':[
-       {'DEVICE':'/dev/ttyReg101', 'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-B12-1_1A'},
-       {'DEVICE':'/dev/ttyReg102', 'M':False, 'PV':'PA-RaPS?:SI-Reg:PS-B12-1_1B'},
-       {'DEVICE':'/dev/ttyReg103', 'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-B12-1_2A'},
-       {'DEVICE':'/dev/ttyReg104', 'M':False, 'PV':'PA-RaPS?:SI-Reg:PS-B12-1_2B'},
-       {'DEVICE':'/dev/ttyReg105', 'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-B12-1_3A'},
-       {'DEVICE':'/dev/ttyReg106', 'M':False, 'PV':'PA-RaPS?:SI-Reg:PS-B12-1_3B'},
-       {'DEVICE':'/dev/ttyReg107', 'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-B12-1_4A'},
-       {'DEVICE':'/dev/ttyReg108', 'M':False, 'PV':'PA-RaPS?:SI-Reg:PS-B12-1_4B'},
-       {'DEVICE':'/dev/ttyReg109', 'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-B12-2_1A'},
-       {'DEVICE':'/dev/ttyReg110', 'M':False, 'PV':'PA-RaPS?:SI-Reg:PS-B12-2_1B'},
-       {'DEVICE':'/dev/ttyReg111', 'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-B12-2_2A'},
-       {'DEVICE':'/dev/ttyReg112', 'M':False, 'PV':'PA-RaPS?:SI-Reg:PS-B12-2_2B'},
-       {'DEVICE':'/dev/ttyReg113', 'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-B12-2_3A'},
-       {'DEVICE':'/dev/ttyReg114', 'M':False, 'PV':'PA-RaPS?:SI-Reg:PS-B12-2_3B'},
-       {'DEVICE':'/dev/ttyReg115', 'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-B12-2_4A'},
-       {'DEVICE':'/dev/ttyReg116', 'M':False, 'PV':'PA-RaPS?:SI-Reg:PS-B12-2_4B'},
+       {'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-B12-1_1A'},
+       {'M':False, 'PV':'PA-RaPS?:SI-Reg:PS-B12-1_1B'},
+       {'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-B12-1_2A'},
+       {'M':False, 'PV':'PA-RaPS?:SI-Reg:PS-B12-1_2B'},
+       {'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-B12-1_3A'},
+       {'M':False, 'PV':'PA-RaPS?:SI-Reg:PS-B12-1_3B'},
+       {'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-B12-1_4A'},
+       {'M':False, 'PV':'PA-RaPS?:SI-Reg:PS-B12-1_4B'},
+       {'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-B12-2_1A'},
+       {'M':False, 'PV':'PA-RaPS?:SI-Reg:PS-B12-2_1B'},
+       {'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-B12-2_2A'},
+       {'M':False, 'PV':'PA-RaPS?:SI-Reg:PS-B12-2_2B'},
+       {'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-B12-2_3A'},
+       {'M':False, 'PV':'PA-RaPS?:SI-Reg:PS-B12-2_3B'},
+       {'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-B12-2_4A'},
+       {'M':False, 'PV':'PA-RaPS?:SI-Reg:PS-B12-2_4B'},
     ]},
     {'file':'st-quadrupoles.cmd', 'items':[
-       {'DEVICE':'/dev/ttyReg117', 'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-QFAP'},
-       {'DEVICE':'/dev/ttyReg118', 'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-QFB'},
-       {'DEVICE':'/dev/ttyReg119', 'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-QDAP12'},
-       {'DEVICE':'/dev/ttyReg120', 'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-QDB12'},
-       {'DEVICE':'/dev/ttyReg121', 'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-Q12_A'},
-       {'DEVICE':'/dev/ttyReg122', 'M':False, 'PV':'PA-RaPS?:SI-Reg:PS-Q12_B'},
-       {'DEVICE':'/dev/ttyReg123', 'M':False, 'PV':'PA-RaPS?:SI-Reg:PS-Q12_C'},
-       {'DEVICE':'/dev/ttyReg124', 'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-Q33_A'},
-       {'DEVICE':'/dev/ttyReg125', 'M':False, 'PV':'PA-RaPS?:SI-Reg:PS-Q33_B'},
-       {'DEVICE':'/dev/ttyReg126', 'M':False, 'PV':'PA-RaPS?:SI-Reg:PS-Q33_C'},
+       {'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-QFAP'},
+       {'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-QFB'},
+       {'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-QDAP12'},
+       {'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-QDB12'},
+       {'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-Q12_A'},
+       {'M':False, 'PV':'PA-RaPS?:SI-Reg:PS-Q12_B'},
+       {'M':False, 'PV':'PA-RaPS?:SI-Reg:PS-Q12_C'},
+       {'M':True,  'PV':'PA-RaPS?:SI-Reg:PS-Q33_A'},
+       {'M':False, 'PV':'PA-RaPS?:SI-Reg:PS-Q33_B'},
+       {'M':False, 'PV':'PA-RaPS?:SI-Reg:PS-Q33_C'},
     ]},
     {'file':'st-sextupoles.cmd', 'items':[
-       {'DEVICE':'/dev/ttyReg127', 'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SDAP0'},
-       {'DEVICE':'/dev/ttyReg128', 'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SDB0'},
-       {'DEVICE':'/dev/ttyReg129', 'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SFAP0'},
-       {'DEVICE':'/dev/ttyReg130', 'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SFB0'},
-       {'DEVICE':'/dev/ttyReg131', 'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SDB1'},
-       {'DEVICE':'/dev/ttyReg132', 'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SDA12'},
-       {'DEVICE':'/dev/ttyReg133', 'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SDA3FA1'},
-       {'DEVICE':'/dev/ttyReg134', 'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SDB2'},
-       {'DEVICE':'/dev/ttyReg135', 'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SFA2DP1'},
-       {'DEVICE':'/dev/ttyReg136', 'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SDB3'},
-       {'DEVICE':'/dev/ttyReg137', 'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SDP23'},
-       {'DEVICE':'/dev/ttyReg138', 'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SFB1'},
-       {'DEVICE':'/dev/ttyReg139', 'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SFP12'},
-       {'DEVICE':'/dev/ttyReg140', 'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SFB2'},
+       {'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SDAP0'},
+       {'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SDB0'},
+       {'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SFAP0'},
+       {'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SFB0'},
+       {'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SDB1'},
+       {'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SDA12'},
+       {'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SDA3FA1'},
+       {'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SDB2'},
+       {'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SFA2DP1'},
+       {'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SDB3'},
+       {'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SDP23'},
+       {'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SFB1'},
+       {'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SFP12'},
+       {'M':True, 'PV':'PA-RaPS?:SI-Reg:PS-SFB2'},
     ]}
 ]
 
@@ -92,9 +92,9 @@ if __name__ == '__main__':
              s_ports, dbs, m_dbs, asyn_dbs = '','','',''
              port = 0
              for item in d['items']:
-                 item['PORT'] = 'P{}'.format(port)
                  port += 1
-                 s_ports += s_port.safe_substitute(**item)
+                 item['PORT'] = 'P{}'.format(port)
+                 s_ports += s_port.safe_substitute(DEVICE='/dev/tty_dgrp_{}_0'.format(port), **item)
                  asyn_dbs += asyn_db.safe_substitute(**item)
                  dbs += db.safe_substitute(**item)
                  if item['M'] == True:

fc37ef3cae0429718a2e5376c466e46269373445
carneirofc <root@Regatron-Quadrupoles.lnls-sirius.com.br>
2019-09-17 14:12:48 -0300

    iocBoot: Quadrupoles config/remove node

---
+++ iocBoot/iocTopCon/config-node-quadrupoles.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -x
+set -e
+
+# Quadrupoles
+/usr/bin/dgrp_cfg_node init 17 10.128.125.117 1
+/usr/bin/dgrp_cfg_node init 18 10.128.125.118 1
+/usr/bin/dgrp_cfg_node init 19 10.128.125.119 1
+/usr/bin/dgrp_cfg_node init 20 10.128.125.120 1
+/usr/bin/dgrp_cfg_node init 21 10.128.125.121 1
+/usr/bin/dgrp_cfg_node init 22 10.128.125.122 1
+/usr/bin/dgrp_cfg_node init 23 10.128.125.123 1
+/usr/bin/dgrp_cfg_node init 24 10.128.125.124 1
+/usr/bin/dgrp_cfg_node init 25 10.128.125.125 1
+/usr/bin/dgrp_cfg_node init 26 10.128.125.126 1
+/usr/bin/dgrp_cfg_node init 27 10.128.125.127 1
+/usr/bin/dgrp_cfg_node init 28 10.128.125.128 1
+/usr/bin/dgrp_cfg_node init 29 10.128.125.129 1
+/usr/bin/dgrp_cfg_node init 30 10.128.125.130 1
+/usr/bin/dgrp_cfg_node init 31 10.128.125.131 1
+/usr/bin/dgrp_cfg_node init 32 10.128.125.132 1
+
---
+++ iocBoot/iocTopCon/remove-node-quadrupoles.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -x
+set -e
+
+
+# Quadrupoles
+/usr/bin/dgrp_cfg_node uninit 17 10.128.125.117 1
+/usr/bin/dgrp_cfg_node uninit 18 10.128.125.118 1
+/usr/bin/dgrp_cfg_node uninit 19 10.128.125.119 1
+/usr/bin/dgrp_cfg_node uninit 20 10.128.125.120 1
+/usr/bin/dgrp_cfg_node uninit 21 10.128.125.121 1
+/usr/bin/dgrp_cfg_node uninit 22 10.128.125.122 1
+/usr/bin/dgrp_cfg_node uninit 23 10.128.125.123 1
+/usr/bin/dgrp_cfg_node uninit 24 10.128.125.124 1
+/usr/bin/dgrp_cfg_node uninit 25 10.128.125.125 1
+/usr/bin/dgrp_cfg_node uninit 26 10.128.125.126 1
+/usr/bin/dgrp_cfg_node uninit 27 10.128.125.127 1
+/usr/bin/dgrp_cfg_node uninit 28 10.128.125.128 1
+/usr/bin/dgrp_cfg_node uninit 29 10.128.125.129 1
+/usr/bin/dgrp_cfg_node uninit 30 10.128.125.130 1
+/usr/bin/dgrp_cfg_node uninit 31 10.128.125.131 1
+/usr/bin/dgrp_cfg_node uninit 32 10.128.125.132 1

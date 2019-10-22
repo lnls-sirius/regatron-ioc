d3f5da00415bc73972ab88c2e918bf6a2f6128ca
carneirofc <claudiofcarneiro@hotmail.com>
2019-10-22 10:34:37 -0300

    Identation and PV name

--- TopConApp/Db/proto_template.py
+++ TopConApp/Db/proto_template.py
@@ -2,25 +2,21 @@
 from string import Template
 
 SELECT = ("""
-LockTimeout     = 10000;
-WriteTimeout    = 10;
-ReplyTimeout    = 1000;
-ReadTimeout     = 10;
-ExtraInput      = Error;
+LockTimeout  = 10000;
+WriteTimeout = 10;
+ReplyTimeout = 1000;
+ReadTimeout  = 10;
+ExtraInput   = Error;
 
 selectSystem {
     ReadTimeout = 5;
     out "\\xA5\\x06\\x11\\xD0\\x50\\x00", "\\x40\\x00", "%2<sum>", "%#/(?sm)(..)(......)(.)/\\1\\3\\2/";
     in  "%#/(?sm)(..)(.)(..)/\\1\\3\\2/", "\\xA5\\x02\\x11\\x00", "%2<sum>";
-#   out "\\xA5\\x04\\x10\\xD0\\x50\\x00", "%2<sum>", "%#/(?sm)(..)(....)(.)/\\1\\3\\2/";
-#   in  "%#/(?sm)(..)(.)(..)(..)/\\1\\3\\4\\2\\4/", "\\xA5\\x04\\x10\\x00\\x40\\x00", "%2<sum>", "%(\\$1:SelIdx-Mon)#02r";
 }
 selectMaster{
     ReadTimeout = 5;
     out "\\xA5\\x06\\x11\\xD0\\x50\\x00", "\\x00\\x00", "%2<sum>", "%#/(?sm)(..)(......)(.)/\\1\\3\\2/";
     in  "%#/(?sm)(..)(.)(..)/\\1\\3\\2/", "\\xA5\\x02\\x11\\x00", "%2<sum>";
-#   out "\\xA5\\x04\\x10\\xD0\\x50\\x00", "%2<sum>", "%#/(?sm)(..)(....)(.)/\\1\\3\\2/";
-#   in  "%#/(?sm)(..)(.)(..)(..)/\\1\\3\\4\\2\\4/", "\\xA5\\x04\\x10\\x00\\x00\\x00", "%2<sum>", "%(\\$1:SelIdx-Mon)#02r";
 }
 getSelIdx{
     MaxInput = 7;

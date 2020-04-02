30566192522b42352dcea55f930357ad6f6c05db
carneirofc <claudiofcarneiro@hotmail.com>
2020-04-02 14:29:22 -0300

    Update test to point at unix socket

--- iocBoot/iocTopCon/st-test.cmd
+++ iocBoot/iocTopCon/st-test.cmd
@@ -1,4 +1,4 @@
-#!../../bin/linux-x86_64/TopCon
+#../../bin/linux-x86_64/TopCon
 
 ## You may have to change TopCon to something else
 ## everywhere it appears in this file
@@ -10,7 +10,9 @@ cd "${TOP}"
 ## Register all support components
 dbLoadDatabase "dbd/TopCon.dbd"
 TopCon_registerRecordDeviceDriver pdbbase
-drvAsynIPPortConfigure("P01", "unix://$(TOP)/test/unix-socket")
+
+drvAsynIPPortConfigure("P01", "unix:///var/run/reg/test.socket")
+
 dbLoadRecords("db/TopCon.db",       "DEVICE=Tc, PORT=P01")
 dbLoadRecords("db/TopConMaster.db", "DEVICE=Tc, PORT=P01")
 dbLoadRecords("db/asynRecord.db",   "P=Tc,R=,PORT=P01,ADDR=,IMAX=,OMAX=")
